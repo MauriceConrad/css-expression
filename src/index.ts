69d5c6f5ae9c3f73cba875b6c5a2bb01d9fe3c85
Maurice Conrad <conr.maur@gmail.com>
2022-06-08 16:42:53 +0200

    export parse arguments and helpers

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 export { default as parse } from './parse'
 export { default as stringify } from './stringify'
+export { default as parseArguments } from './parseArguments'
+export { default as parseExpression } from './parseExpression'
+export { default as parsePrimitive } from './parsePrimitive'
 
 export { ICSSLiteral, ICSSExpression, ICSSPrimitive, ICSSFunction } from './types'
